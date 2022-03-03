b5e32423d446aef92f36d52c27f88ba9a50bc32c
Veronique Jakobs <vmjakobs@gmail.com>
2022-03-03 12:20:05 +0100

    getYieldForPlant working

---
+++ farm.test.js
@@ -0,0 +1,15 @@
+const { 
+    getYieldForPlant
+} = require("./farm");
+
+//getYieldForPlant
+describe("getYieldForPlant", () => {
+    const corn = {
+        name: "corn",
+        yield: 30,
+    };
+
+    test("Get yield for plant with no environment factors", () => {
+        expect(getYieldForPlant(corn)).toBe(30);
+    });
+});
